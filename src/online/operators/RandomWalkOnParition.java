b4b0cdd508eb0acfde3c7921e09bc80bd2512972
Remco Bouckaert <higgs.ml@gmail.com>
2021-04-06 13:35:05 +1200

    add partiton operators

---
+++ src/online/operators/RandomWalkOnParition.java
@@ -0,0 +1,72 @@
+package online.operators;
+
+import beast.core.Description;
+import beast.core.Input;
+import beast.core.Operator;
+import beast.core.Input.Validate;
+import beast.core.parameter.IntegerParameter;
+import beast.evolution.tree.TreeInterface;
+import beast.util.Randomizer;
+
+@Description("A random walk operator that selects a random dimension of the integer parameter from a partition "
+		+ "and perturbs the value a random amount within +/- windowSize.")
+public class RandomWalkOnParition extends Operator implements PartitionOperator {
+	final public Input<TreePartition> partitionInput = new Input<>("partition", "specifies part of the tree to be operated on");
+    final public Input<IntegerParameter> parameterInput = new Input<>("parameter", "the parameter that is operated on", Validate.REQUIRED);
+    final public Input<Integer> windowSizeInput =
+            new Input<Integer>("windowSize", "the size of the window both up and down when using uniform interval OR standard deviation when using Gaussian", Input.Validate.REQUIRED);
+
+    private TreePartition partition;
+    private int windowSize;
+	
+    public RandomWalkOnParition() {}
+    public RandomWalkOnParition(TreePartition partition, IntegerParameter parameter, double weight) {
+    	this(partition, parameter, 1, weight);
+    }
+    public RandomWalkOnParition(TreePartition partition, IntegerParameter parameter, int windowSize, double weight) {
+    	initByName("partition", partition, "parameter", parameter, "windowSize", windowSize, "weight", weight);
+    }
+ 
+    
+    @Override
+	public void initAndValidate() {
+        windowSize = windowSizeInput.get();
+        partition = partitionInput.get();
+    }
+
+    /**
+     * override this for proposals,
+     * returns log of hastingRatio, or Double.NEGATIVE_INFINITY if proposal should not be accepted *
+     */
+    @Override
+    public double proposal() {
+
+        final IntegerParameter param = parameterInput.get(this);
+
+        final int i = param.getDimension() %2 == 0 ?
+        				partition.getRandomNodeNotRoot() :
+        				partition.getRandomNode();
+        
+        final int value = param.getValue(i);
+        final int newValue = value + Randomizer.nextInt(2 * windowSize + 1) - windowSize;
+
+        if (newValue < param.getLower() || newValue > param.getUpper()) {
+            // invalid move, can be rejected immediately
+            return Double.NEGATIVE_INFINITY;
+        }
+        if (newValue == value) {
+            // this saves calculating the posterior
+            return Double.NEGATIVE_INFINITY;
+        }
+
+        param.setValue(i, newValue);
+
+        return 0.0;
+    }
+
+    @Override
+    public void optimize(final double logAlpha) {
+        // nothing to optimise
+    }
+
+}
---
+++ src/online/operators/RateScaleOnPartition.java
@@ -0,0 +1,115 @@
+package online.operators;
+
+import java.text.DecimalFormat;
+
+import beast.core.Description;
+import beast.core.Input;
+import beast.core.Input.Validate;
+import beast.core.Operator;
+import beast.core.parameter.RealParameter;
+import beast.evolution.tree.Node;
+import beast.evolution.tree.TreeInterface;
+import beast.util.Randomizer;
+
+@Description("Scale rate parameter for relaxed clock, but only change values in a given tree partition")
+public class RateScaleOnPartition extends Operator implements PartitionOperator {
+	final public Input<TreePartition> partitionInput = new Input<>("partition", "specifies part of the tree to be operated on");
+    public final Input<RealParameter> parameterInput = new Input<>("parameter", "the parameter that is operated on", Validate.REQUIRED);
+    public final Input<Double> scaleFactorInput = new Input<>("scaleFactor", "scaling factor: larger means more bold proposals", 0.75);
+    final public Input<Double> scaleUpperLimit = new Input<>("upper", "Upper Limit of scale factor", 1.0 - 1e-8);
+    final public Input<Double> scaleLowerLimit = new Input<>("lower", "Lower limit of scale factor", 1e-8);
+
+    TreePartition partition;
+    RealParameter parameter;
+    double scaleFactor;
+    private double upper, lower;
+    TreeInterface tree;
+	
+    public RateScaleOnPartition() {}
+    public RateScaleOnPartition(TreePartition partition, RealParameter parameter, double weight) {
+    	this(partition, parameter, 0.75, weight);
+    }
+    public RateScaleOnPartition(TreePartition partition, RealParameter parameter, double scaleFactor, double weight) {
+    	initByName("partition", partition, "parameter", parameter, "scaleFactor", scaleFactor, "weight", weight);
+    }
+    
+	@Override
+	public void initAndValidate() {
+		partition = partitionInput.get();
+		parameter = parameterInput.get();
+		scaleFactor = scaleFactorInput.get();
+		tree = partition.treeInput.get();
+        upper = scaleUpperLimit.get();
+        lower = scaleLowerLimit.get();
+	}
+
+	@Override
+	public double proposal() {
+		// find suitable rate to scale
+        int i = parameter.getDimension() %2 == 0 ?
+				partition.getRandomNodeNotRoot() :
+				partition.getRandomNode();
+				
+		// do the scaling
+		double scaleOne = getScaler();
+        final double newValue = scaleOne * parameter.getValue(i);
+        double hastingsRatio = - Math.log(scaleOne);
+        if( outsideBounds(newValue, parameter) ) {
+            return Double.NEGATIVE_INFINITY;
+        }
+        parameter.setValue(i, newValue);
+		return hastingsRatio;
+	}
+	
+    protected boolean outsideBounds(final double value, final RealParameter param) {
+        final Double l = param.getLower();
+        final Double h = param.getUpper();
+
+        return (value < l || value > h);
+    }
+
+    protected double getScaler() {
+        return (scaleFactor + (Randomizer.nextDouble() * ((1.0 / scaleFactor) - scaleFactor)));
+    }
+
+
+    /**
+     * automatic parameter tuning *
+     */
+    @Override
+    public void optimize(final double logAlpha) {
+        double delta = calcDelta(logAlpha);
+        delta += Math.log(1.0 / scaleFactor - 1.0);
+        setCoercableParameterValue(1.0 / (Math.exp(delta) + 1.0));
+    }
+
+    @Override
+    public double getCoercableParameterValue() {
+        return scaleFactor;
+    }
+
+    @Override
+    public void setCoercableParameterValue(final double value) {
+        scaleFactor = Math.max(Math.min(value, upper), lower);
+    }
+
+    @Override
+    public String getPerformanceSuggestion() {
+        final double prob = m_nNrAccepted / (m_nNrAccepted + m_nNrRejected + 0.0);
+        final double targetProb = getTargetAcceptanceProbability();
+
+        double ratio = prob / targetProb;
+        if (ratio > 2.0) ratio = 2.0;
+        if (ratio < 0.5) ratio = 0.5;
+
+        // new scale factor
+        final double sf = Math.pow(scaleFactor, ratio);
+
+        final DecimalFormat formatter = new DecimalFormat("#.###");
+        if (prob < 0.10) {
+            return "Try setting scaleFactor to about " + formatter.format(sf);
+        } else if (prob > 0.40) {
+            return "Try setting scaleFactor to about " + formatter.format(sf);
+        } else return "";
+    }
+}
